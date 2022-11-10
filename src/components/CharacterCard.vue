5138661155f8821bc1f2856effaaea7373b7aaff
Mirko Robbi <mirko.robbi@gmail.com>
2022-11-10 15:54:46 +0100

    card responsive

--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -8,13 +8,35 @@ export default {
 </script>
 
 <template>
-    <div class="character-item text-center">
+    <div class="character-item">
         <img :src="character.img" alt="">
-        <h4>{{ character.name }}</h4>
-        <h4>ciao</h4>
+        <div class="text d-flex flex-column align-items-center">
+            <h4>{{ character.name }}</h4>
+            <h4>{{ character.status }}</h4>
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
+.character-item {
+    background-color: #2e3a46;
+    padding: 15px;
+    height: 100%;
 
+    .text {
+        margin-top: 20px;
+
+        h4 {
+            color: white;
+            margin-top: 0.6rem;
+
+
+            &:last-child {
+                color: #716b67;
+            }
+        }
+    }
+
+
+}
 </style>
--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -15,15 +15,26 @@ export default {
 </script>
 
 <template>
-    <h2 class="text-center">Lista personaggi breaking bad</h2>
 
-    <div class="row row-cols-2 row-cols-md-4 g-3">
-        <div class="col" v-for="character in store.characters" :key="character.id">
-            <CharacterCard :character="character" />
+    <div class="container">
+        <div class="wrapper">
+            <div class="row row-cols-2 row-cols-md-4 row-cols-ld-5 g-3">
+                <div class="col" v-for="character in store.characters" :key="character.id">
+                    <CharacterCard :character="character" />
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
+.container {
+    background-color: white;
 
+    .wrapper {
+        width: 90%;
+        margin: 0 auto;
+        padding-top: 80px;
+    }
+}
 </style>
