c1a2ad1ea6be99b2a0db11ffc7285a31fd2e3df0
Mirko Robbi <mirko.robbi@gmail.com>
2022-11-13 13:09:59 +0100

    add logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -12,12 +12,18 @@ export default {
 
 <template>
     <header>
+        <div class="logo">
+            <img src="../assets/logo.png" alt="">
+        </div>
         <h1 class=" text-start header-title mb-4">Breaking Bad Api</h1>
-        <div class="container select-bar">
-            <select @change="$emit('performSearch')" v-model="store.searchCategory">
-                <option value="Breaking Bad">Breaking Bad</option>
-                <option value="Better Call Saul">Better call Saul</option>
-            </select>
+        <div class="container ">
+            <div class="select-bar">
+
+                <select @change="$emit('performSearch')" v-model="store.searchCategory">
+                    <option value="Breaking Bad">Breaking Bad</option>
+                    <option value="Better Call Saul">Better call Saul</option>
+                </select>
+            </div>
         </div>
     </header>
 </template>
@@ -34,9 +40,19 @@ header {
     background-image: linear-gradient(to bottom right, #16191b, #394551a1);
     border-bottom: 1px solid black;
 
+    .logo {
+        max-width: 200px;
+        display: inline-block;
+    }
+
     h1 {
         color: white;
         padding: 10px;
+        display: inline-block;
+    }
+
+    .select-bar {
+        margin-right: 100px;
     }
 }
 </style>
